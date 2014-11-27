905ca5bad26e1f2c823355884d70f4ee04393ed4
Vaclav Rehak <vaclav.rehak@auto-mat.cz>
2014-11-27 10:15:28 +0100

    added error handling

--- run.py
+++ run.py
@@ -5,19 +5,25 @@ import django
 django.setup()
 
 import json
+import logging
 from tsk.models import Lokace, Provoz
+
+logger = logging.getLogger(__name__)
 
 indata = open('stupneprovozu.json').read()
 data = json.loads(indata)
 
 for i in data['results']:
-    lokace, created = Lokace.objects.get_or_create(name=i['location'])
-    provoz, created = Provoz.objects.get_or_create(
-        ident=i['id'],
-        location=lokace,
-        level=i['level'],
-        time_generated=i['timeGenerated'],
-        time_start=i['timeStart'],
-        time_stop=i['timeStop'],
-    )
+    try:
+        lokace, created = Lokace.objects.get_or_create(name=i['location'])
+        provoz, created = Provoz.objects.get_or_create(
+            ident=i['id'],
+            location=lokace,
+            level=i['level'],
+            time_generated=i['timeGenerated'],
+            time_start=i['timeStart'],
+            time_stop=i['timeStop'],
+        )
+    except Exception,e:
+        logger.exception('Error parsing TSK data: %s' % i)
 
