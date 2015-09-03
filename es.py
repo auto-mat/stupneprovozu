9bc0f851642ed43952e9a129d94c0f7f799c5e0b
Vaclav Rehak <vaclav.rehak@auto-mat.cz>
2015-09-03 23:06:25 +0200

    ElasticSearch import improvements
    Thx @HonzaKral

--- es.py
+++ es.py
@@ -7,23 +7,43 @@ django.setup()
 from tsk.models import Provoz 
 
 from datetime import datetime
-from elasticsearch import Elasticsearch
-es = Elasticsearch()
+
+from elasticsearch.helpers import streaming_bulk
+
+from elasticsearch_dsl import DocType, Object, String, GeoPoint, Date, Integer, Float
+from elasticsearch_dsl.connections import connections
+
+es = connections.create_connection()
+
+class TrafficReport(DocType):
+    location = Object(properties={
+        'name': String(fields={'raw': String(index='not_analyzed')}),
+        'geo': GeoPoint()
+    })
+    timestamp = Date()
+
+    class Meta:
+        index = 'traffic'
+
+def get_provoz():
+    for p in Provoz.objects.all().select_related('location')[:500000].iterator():
+        yield {
+            'location': {'name': p.location.name, 'geo': {'lat': 0, 'lon': 0}},
+            'timestamp': p.time_start,
+            'traffic': p.level,
+            'cas': float(p.time_start.strftime('%H')) + float(p.time_start.strftime('%M'))/60,
+            'den_v_tydnu': p.time_start.weekday(),
+            '_id': p.id
+        }
 
 def django_import():
-    from django.core.paginator import Paginator
-    paginator = Paginator(Provoz.objects.all(), 1000)
-
-    for page in range(1, paginator.num_pages + 1):
-        print 'Loading page (1000 objs) no.', page
-        for p in paginator.page(page).object_list:
-            doc = {
-                'location': {'name': p.location.name, 'geo': {'lat': 0, 'lon': 0}},
-                'timestamp': p.time_start,
-                'traffic': p.level
-            }
-            res = es.index(index="traffic", doc_type='traffic_report', id=p.id, body=doc)
-            #print(res['created'])
+    #es.indices.delete(index='traffic', ignore=404)
+    #TrafficReport.init()
+    i = 0
+    for ok, info in streaming_bulk(es, get_provoz(), doc_type='traffic_report', index='traffic'):
+        i+= 1
+        if i % 1000 == 0:
+            print(i, 'dokumentu hotovo')
 
 def search():
     es.indices.refresh(index="traffic")
