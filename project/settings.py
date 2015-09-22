ce0f22c8915032a56cc580eba3d8d1a63adb1e12
Petr Dlouhý <petr.dlouhy@email.cz>
2015-09-22 12:17:18 +0200

    TimeFilter and DateRangeFilter

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tsk',
+    'daterange_filter',
 )
 
 MIDDLEWARE_CLASSES = (
--- tsk/admin.py
+++ tsk/admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from daterange_filter.filter import DateRangeFilter
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
 from .models import Lokace, Provoz
@@ -21,9 +22,32 @@ class LocationFilter(SimpleListFilter):
             return queryset
 
 
+class TimeFilter(SimpleListFilter):
+    title = "Časové období"
+    parameter_name = "time"
+
+    def lookups(self, request, model_admin):
+        list_tuple = []
+        for location in Lokace.objects.filter(favourite=True):
+            list_tuple.append((location.id, location.name))
+        return [
+            ('rano', 'ráno (8-9)'),
+            ('odpoledne', 'odpoledne (17-18)'),
+            ]
+
+
+    def queryset(self, request, queryset):
+        if self.value() == 'rano':
+            return queryset.filter(time_start__hour=8)
+        elif self.value() == 'odpoledne':
+            return queryset.filter(time_start__hour=17)
+        else:
+            return queryset
+
+
 class ProvozAdmin(admin.ModelAdmin):
-    list_display = ('location', 'level', 'time_generated',)
-    list_filter = (LocationFilter,)
+    list_display = ('location', 'level', 'time_generated', 'time_start', 'time_stop')
+    list_filter = (LocationFilter, TimeFilter, ('time_start', DateRangeFilter))
 
 
 class LokaceAdmin(admin.ModelAdmin):
