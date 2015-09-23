0e50ad1ba1a58d7a80260f7ec67ead4d55cd2465
Petr Dlouhý <petr.dlouhy@email.cz>
2015-09-23 11:52:52 +0200

    congestion rate action

--- tsk/admin.py
+++ tsk/admin.py
@@ -3,6 +3,8 @@
 from daterange_filter.filter import DateRangeFilter
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
+from django.db.models import Count
+from django.utils.safestring import mark_safe
 from .models import Lokace, Provoz
 
 class LocationFilter(SimpleListFilter):
@@ -44,14 +46,26 @@ class TimeFilter(SimpleListFilter):
         else:
             return queryset
 
+def show_levels(modeladmin, request, queryset):
+    levels = dict(queryset.values('level').annotate(level_count = Count('id')).order_by().values_list('level', 'level_count'))
+    count = queryset.count()
+    level4 = levels[4] if 4 in levels else 0
+    level5 = levels[5] if 5 in levels else 0
+    congest_rate = (float(level4 + level5) / count) * 100.0
+    modeladmin.message_user(request, mark_safe("Stupně provozu: %s<br/>Počet: %s<br/>Podíl zácpy: %.2f%%" % (levels, count, congest_rate)))
+show_levels.short_description = "Ukázat stupně provozu"
+
 
 class ProvozAdmin(admin.ModelAdmin):
     list_display = ('location', 'level', 'time_generated', 'time_start', 'time_stop')
-    list_filter = (LocationFilter, TimeFilter, ('time_start', DateRangeFilter))
+    list_filter = (LocationFilter, TimeFilter, ('time_start', DateRangeFilter), 'level')
+    search_fields = ('location__name',)
+    actions = (show_levels,)
 
 
 class LokaceAdmin(admin.ModelAdmin):
     list_display = ('name', 'favourite',)
+    search_fields = ('name',)
     list_filter = ('favourite',)
 
 admin.site.register(Lokace, LokaceAdmin)
